c6c3a9a498438dcc4178cdade5dbf5860583057d
Amr Samy <41027803+AmrSamyGat@users.noreply.github.com>
2023-11-29 03:23:53 +0200

    u

--- Views/View.py
+++
@@ -1,8 +0,0 @@
-import discord
-from .Arrows import ArrowButton
-from .SearchButton import SearchButton
-from Map import Map
-
-class CUFEView(discord.ui.View):
-    def __init__(self):
-        super().__init__()
